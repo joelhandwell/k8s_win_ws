b9f062c23f91394ae3cd25aaccf05b3abac95545
Joel Handwell <joelhandwell@gmail.com>
2017-02-07 12:07:20 -0500

    Add refreshenv

--- win_ws.bat
+++ win_ws.bat
@@ -1,4 +1,8 @@
+REM Kubernetes essentials
 choco install virtualbox -y
 choco install minikube -y
 choco install kubernetes-cli -y
 choco install kubernetes-kompose -y
+
+REM update environment variables
+REFRESHENV
