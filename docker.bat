adb0aeef0683b347e7ada2813461658fe1ed6147
Joel Handwell <joelhandwell@gmail.com>
2018-08-23 16:01:14 -0400

    Add skaffold and remove outdated tools

--- docker.bat
+++
@@ -1,8 +0,0 @@
-REM docker essentials
-choco install virtualbox -y
-choco install docker -y
-choco install docker-machine -y
-choco install docker-compose -y
-
-REM update environment variables
-REFRESHENV
--- win_ws.bat
+++ win_ws.bat
@@ -1,7 +1,15 @@
+REM SysAdmin
+choco install ConEmu clink fd putty mtputty rsync hosts.editor hashcheck keepass pwgen greenshot windirstat -y
+
+REM DevTools
+choco install vim sublimetext3 kdiff3 -y
+choco install git.install -y -params '/GitAndUnixToolsOnPath'
+
+REM If not Windows 10 Pro
+choco install virtualbox -y
+
 REM Kubernetes essentials
-choco install minikube -y
-choco install kubernetes-cli -y
-choco install kubernetes-kompose -y
+choco install minikube kubernetes-cli skaffold -y
 
 REM update environment variables
 REFRESHENV
